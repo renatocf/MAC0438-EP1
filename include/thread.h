c63214d30a9a2c6319b0783fac5f5a4fa1bd34f5
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-04-21 14:45:09 -0300

    Add comments

--- include/thread.h
+++ include/thread.h
@@ -9,17 +9,27 @@
 typedef void *(pthread_action_t)(void *);
 typedef void *pthread_arg_t;
 
-/* Semaphore array */
+/*---------------------------------------------------------------------------*/
+/*                            SEMAPHORE ARRAY                                */
+/*---------------------------------------------------------------------------*/
+
+/* Types */
 typedef struct _sem_array_t *sem_array_t;
 
+/* Functios */
 sem_array_t sem_array_create(
   unsigned int n, int pshared, unsigned int value);
 
 void sem_array_destroy(sem_array_t sems);
 
-/* Thread array */
+/*---------------------------------------------------------------------------*/
+/*                             THREAD ARRAY                                  */
+/*---------------------------------------------------------------------------*/
+
+/* Types */
 typedef struct _pthread_array_t *pthread_array_t;
 
+/* Functios */
 pthread_array_t pthread_array_create(
   unsigned int n, pthread_action_t action, pthread_arg_t args);
 
