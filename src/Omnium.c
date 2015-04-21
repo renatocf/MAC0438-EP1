f5f80139e5062ab164240e96b5d69a14c6db534d
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-04-21 11:27:59 -0300

    Organize code and create new functions

--- src/Omnium.c
+++ src/Omnium.c
@@ -22,42 +22,48 @@ int sort_initial_position() {
   return generate_aleatory(0, distance);
 }
 
-void *perform_work(void *argument) {
-
+void d_start(int i) {
   time_t now;
-  int i = *((int *) argument);
-  int position = sort_initial_position();
-
   time(&now);
   printf("thread%d starting at %s", i, ctime (&now));
+}
 
-  sleep(i);
-  pthread_barrier_wait(&barrier);
-
+void d_end(int i) {
+  time_t now;
   time(&now);
   printf ("barrier in thread%d() done at %s", i, ctime(&now));
+}
 
-  printf("Hello World! It's me, thread with initial position %d!\n",
-      position);
+void *perform_work(void *argument) {
 
+  /* Initialize *******************************************************/
+  int i = *((int *) argument);
+  int position = sort_initial_position();
+  d_start(i); /* TODO: remove */
+
+  /** Process *********************************************************/
+  printf("thread[%d]: initial position = %d!\n", i, position);
+  sleep(i);
+
+  pthread_barrier_wait(&barrier);
+
+  /* End **************************************************************/
+  d_end(i); /* TODO: remove */
   return NULL;
 }
 
 void simulate_race() {
 
-  pthread_array_t threads;
-
-  printf("d:%d n:%d uniforme:%d\n", distance, n_cyclists, uniforme);
-
-  threads = pthread_array_create(n_cyclists, perform_work, NULL);
-
+  /* Initialize *******************************************************/
+  pthread_array_t threads = pthread_array_create(n_cyclists, perform_work, NULL);
   pthread_barrier_init (&barrier, NULL, n_cyclists + 1);
 
+  /** Process *********************************************************/
   pthread_barrier_wait(&barrier);
   pthread_array_join(n_cyclists, threads);
 
+  /* End **************************************************************/
   pthread_array_destroy(threads);
-
   printf("In main: All threads completed successfully\n");
 }
 
@@ -86,6 +92,8 @@ int main(int argc, char **argv) {
   }
 
   srand(time(NULL));
+
+  printf("d:%d n:%d uniforme:%d\n", distance, n_cyclists, uniforme);
 
   simulate_race();
 
